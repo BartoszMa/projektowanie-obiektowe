0e341a2e327eade1968cd1ebe44f53230022cbbd
Barti <bartosz.majkowski@wp.pl>
2025-03-20 17:24:33 +0100

    zad pascal 3.5

--- lab1_pascal/rand.pas
+++ lab1_pascal/rand.pas
@@ -10,11 +10,39 @@ begin
 
     for i := 0 to 49 do
         data[i] := random(100);
-
-    for i := 0 to 49 do
-        writeln(data[i]);
 end;
+
+procedure sortNum();
+var
+    i, j, tmp: Integer;
+begin
+    for i := 0 to 49 do
+        for j := 0 to 49 - i - 1 do
+        begin
+            if data[j] > data[j+1] then
+            begin
+                 tmp := data[j];
+                 data[j] := data[j+1];
+                 data[j+1] := tmp;
+            end;
+        end;
+end;
+
+var
+    q: Integer;
 
 begin
     randNum;
+
+    writeln('Before sort');
+
+    for q := 0 to 49 do
+        writeln(data[q]);
+
+    writeln('After sort');
+
+    sortNum;
+
+    for q := 0 to 49 do
+        writeln(data[q]);
 end.
