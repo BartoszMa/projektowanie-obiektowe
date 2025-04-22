4ecd2a78b46fbbefec51a157608ddcd3f564df7d
Barti <bartosz.majkowski@wp.pl>
2025-04-22 21:45:57 +0200

    lab4 4

--- lab4/handlers/handlers.go
+++ lab4/handlers/handlers.go
@@ -1,63 +1,28 @@
 package handlers
 
 import (
-	"encoding/json"
+	"fmt"
 	"github.com/labstack/echo"
 	"net/http"
+	"os"
 )
 
 func WeatherHandler(c echo.Context) error {
-	sampleResponse := `
-{
-    "location": {
-        "name": "Gdynia",
-        "region": "",
-        "country": "Poland",
-        "lat": 54.5,
-        "lon": 18.55,
-        "tz_id": "Europe/Warsaw",
-        "localtime_epoch": 1745347628,
-        "localtime": "2025-04-22 20:47"
-    },
-    "current": {
-        "last_updated_epoch": 1745347500,
-        "last_updated": "2025-04-22 20:45",
-        "temp_c": 9.3,
-        "temp_f": 48.7,
-        "is_day": 0,
-        "condition": {
-            "text": "Mist",
-            "icon": "//cdn.weatherapi.com/weather/64x64/night/143.png",
-            "code": 1030
-        },
-        "wind_mph": 2.5,
-        "wind_kph": 4.0,
-        "wind_degree": 103,
-        "wind_dir": "ESE",
-        "pressure_mb": 1017.0,
-        "pressure_in": 30.03,
-        "precip_mm": 0.0,
-        "precip_in": 0.0,
-        "humidity": 93,
-        "cloud": 0,
-        "feelslike_c": 9.3,
-        "feelslike_f": 48.8,
-        "windchill_c": 8.0,
-        "windchill_f": 46.3,
-        "heatindex_c": 8.1,
-        "heatindex_f": 46.6,
-        "dewpoint_c": 7.6,
-        "dewpoint_f": 45.6,
-        "vis_km": 10.0,
-        "vis_miles": 6.0,
-        "uv": 0.0,
-        "gust_mph": 5.2,
-        "gust_kph": 8.3
-    }
-}`
+	city := c.QueryParam("city")
+	apiKey := os.Getenv("WEATHER_API_KEY")
 
-	var response map[string]interface{}
-	json.Unmarshal([]byte(sampleResponse), &response)
+	url := fmt.Sprintf("http://api.weatherapi.com/v1/current.json?key=%s&q=%s&aqi=no", apiKey, city)
+	fmt.Println(url)
 
-	return c.JSON(http.StatusOK, response)
+	resp, err := http.Get(url)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, "Failed to fetch weather data")
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return c.String(resp.StatusCode, "Error from weather API")
+	}
+
+	return c.Stream(resp.StatusCode, "application/json", resp.Body)
 }
--- lab4/main.go
+++ lab4/main.go
@@ -1,14 +1,16 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 	"lab4/handlers"
 )
 
 func main() {
 	e := echo.New()
+	godotenv.Load()
 
-	e.GET("/", handlers.WeatherHandler)
+	e.GET("/weather", handlers.WeatherHandler)
 
 	e.Logger.Fatal(e.Start(":5100"))
 }
